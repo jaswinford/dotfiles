d8e019f79468b1fe75224cbaa4d7f56363e2f1de
Adam Swinford <aswinford@jadam.space>
2025-07-22 09:29:37 -0400

    Continued tweaking of nvim config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,16 @@
 return {
-	"nvim-treesitter/nvim-treesitter", 
-	branch = 'main', 
-	lazy = false, 
-	build = ":TSUpdate",
-  ensure_installed = "all",
-	highlight = { enable = true },
-	incremental_selection = { enable = true },
-	textobjects = { enable = true },
+	'nvim-treesitter/nvim-treesitter',
+	branch = 'master',
+	lazy = false,
+	build = ':TSUpdate',
+	opts = {
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+		},
+	},
+	config = function(_,opts)
+		require('nvim-treesitter.configs').setup(opts)
+	end
 }
